91be9bc46e0543fb2c5c1cb03c00cb6170829f65
Jurnell Cockhren <jurnell@sophicware.com>
2017-02-28 17:32:14 -0800

    Fake Blog Exercise Boilerplate

---
+++ FakeBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FakeBlog.Startup))]
+namespace FakeBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
